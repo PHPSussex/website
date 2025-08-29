7606a681d6724c891bafaba7c7fb8bbe29562da0
Joby Harding <code@fullstackappco.com>
2025-08-29 16:18:47 +0100

    Explicit error message for missing settings

--- app/functions.php
+++ app/functions.php
@@ -2,11 +2,17 @@
 
 namespace App;
 
+use Exception;
 use Illuminate\Support\Str;
 
 function setting($name): mixed
 {
     $class = Str::of($name)->studly()->prepend('App\\Models\\Settings\\')->value();
+    $instance = $class::first();
 
-    return $class::first()->value;
+    if (is_null($instance)) {
+        throw new Exception("Setting '$name' not found.");
+    }
+
+    return $instance->value;
 }
