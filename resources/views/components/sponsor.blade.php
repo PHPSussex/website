0bf102ed5d404d5224b28d9c6ba0afa30b122288
Joby Harding <code@fullstackappco.com>
2025-09-01 16:00:54 +0100

    Update sponsor logo color

--- resources/views/components/sponsor.blade.php
+++ resources/views/components/sponsor.blade.php
@@ -3,7 +3,7 @@
     <span class="block text-sm">{{ $provides }}</span>
     <x-dynamic-component
         component="logo.{{ Str::of($name)->kebab()->value() }}"
-        @class(['h-[3.5rem] text-primary-600 my-1.5', $logoClass])
+        @class(['h-[3.5rem] text-mono-300 my-1.5', $logoClass])
     />
     <a class="text-xs link link-focus" href="{{ $url }}">{{ $url }}</a>
 </div>
