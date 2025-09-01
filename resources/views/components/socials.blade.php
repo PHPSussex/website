fff65acac3de32ae39e89209374c69ce8a96256d
Joby Harding <code@fullstackappco.com>
2025-09-01 15:51:28 +0100

    Tweak socials hover states

--- resources/views/components/socials.blade.php
+++ resources/views/components/socials.blade.php
@@ -9,15 +9,17 @@
                 href="{{ $social->url() }}"
                 @class([
                     'group flex items-center justify-center',
-                    'p-2 py-4 md:py-6 lg:py-8 bg-gray-950/0 hover:bg-mono-900',
+                    'p-2 py-4 md:py-6 lg:py-8 bg-gray-950/0 hover:bg-mono-950',
                     'focus-visible:outline-none focus-visible:bg-amber-500',
                     'focus-visible:text-mono-950',
+                    'hover:text-mono-100',
                 ])
             >
                 <div class="translate-y-2 flex flex-col items-center gap-1">
                     <x-dynamic-component :component="$social->icon()" class="w-12 h-12" />
                     <x-type variant="dim" @class([
                         'text-sm group-focus-visible:text-mono-950',
+                        'group-hover:text-mono-300',
                     ])>{{ $social->label() }}</x-type>
                 </div>
             </a>
