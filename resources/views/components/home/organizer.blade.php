d2cff64e04fb014169b049c309bb9c8c3e8f9ca4
Joby Harding <code@fullstackappco.com>
2025-09-01 10:31:01 +0100

    Fix Safari blend bug

--- resources/views/components/home/organizer.blade.php
+++ resources/views/components/home/organizer.blade.php
@@ -4,8 +4,9 @@
         <x-type variant="primary" class="block @min-[275px]:inline italic text-nowrap">"{{ $pronounced }}"</x-type>
         <x-type variant="dim" class="block @min-[400px]:inline">[he/him]</x-type>
     </p>
-    <div class="w-36 h-36 rounded-full border border-mono-800 overflow-hidden bg-gradient-to-br from-primary-200 to-primary-700">
-        <img alt="Portrait of Yannick Chenot" src="{{ $img }}" class="grayscale mix-blend-multiply" />
+    <div class="w-36 h-36 rounded-full border border-mono-800 overflow-hidden bg-gradient-to-br from-primary-200 to-primary-700 relative">
+        <img alt="Portrait of Yannick Chenot" src="{{ $img }}" class="grayscale object-cover w-full h-full" />
+        <div class="absolute inset-0 bg-gradient-to-br from-primary-200 to-primary-700 mix-blend-multiply"></div>
     </div>
     <a class="link link-focus text-lg" href="{{ $linkedin }}">LinkedIn</a>
 </address>
