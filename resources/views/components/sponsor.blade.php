7996502cb148a1db32d92d59dabb91dd80e525f8
Joby Harding <code@fullstackappco.com>
2025-09-01 15:39:00 +0100

    Tweak sponsors section

--- resources/views/components/sponsor.blade.php
+++ resources/views/components/sponsor.blade.php
@@ -3,7 +3,7 @@
     <span class="block text-mono-300">{{ $provides }}</span>
     <x-dynamic-component
         component="logo.{{ Str::of($name)->kebab()->value() }}"
-        @class(['h-[3.5rem] text-mono-600 my-1', $logoClass])
+        @class(['h-[3.5rem] text-primary-600 my-1.5', $logoClass])
     />
     <a class="text-sm link link-focus" href="{{ $url }}">{{ $url }}</a>
 </div>
