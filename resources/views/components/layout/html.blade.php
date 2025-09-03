9aa341488f963986e827713cda70011e5dc144d9
Joby Harding <code@fullstackappco.com>
2025-09-03 16:35:46 +0100

    Add description and twitter card meta

--- resources/views/components/layout/html.blade.php
+++ resources/views/components/layout/html.blade.php
@@ -1,3 +1,4 @@
+@use(Illuminate\Support\Facades\Vite)
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full sm:text-lg xl:text-xl">
 <head>
@@ -5,6 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>PHP Sussex</title>
+
+    @php
+        $description = 'PHP Sussex is a free, open to all, monthly meetup based in Brighton, UK.';
+        $banner = Vite::asset('resources/images/banner.png');
+    @endphp
+    <meta name="description" content="{{ $description }}">
+    <meta property="og:image" content="{{ $banner }}">
+    <meta name="twitter:description" content="{{ $description }}">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@PHPSussexUG">
+    <meta name="twitter:creator" content="@PHPSussexUG">
+    <meta name="twitter:title" content="{{ $description }}">
+    <meta name="twitter:image" content="{{ $banner }}">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
