c8c22eff579c90c9df2cdb889cd4698a86d0d0e6
Joby Harding <code@fullstackappco.com>
2025-09-05 12:59:02 +0100

    Tweak paragraph component

--- resources/views/components/type/para.blade.php
+++ resources/views/components/type/para.blade.php
@@ -5,6 +5,4 @@ $variantClasses = variant($variant, [
     'small' => 'leading-relaxed'
 ])
 @endphp
-<x-type
-    {{ $attributes->merge(['tag' => 'p', 'variant' => $variant])->class(['md:leading-loose mb-3', ...$variantClasses]) }}
->{{ $slot }}</x-type>
+<x-type variant="{{ $variant }}" {{ $attributes->merge(['tag' => 'p'])->class(['md:leading-loose mb-3', ...$variantClasses]) }}>{{ $slot }}</x-type>
