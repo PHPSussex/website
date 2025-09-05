d83b432eaa8fbdab9b89b81a6caafd70ab7e5328
Joby Harding <code@fullstackappco.com>
2025-09-05 08:25:50 +0100

    Responsive tweak for small paragraphs

--- resources/views/components/type/para.blade.php
+++ resources/views/components/type/para.blade.php
@@ -2,7 +2,7 @@
 @use(function App\variant)
 @php
 $variantClasses = variant($variant, [
-    'small' => 'text-xs !leading-relaxed'
+    'small' => 'text-sm lg:text-xs !leading-relaxed'
 ])
 @endphp
 <x-type tag="p" variant="dim" {{ $attributes->class(['md:leading-loose mb-3', ...$variantClasses]) }}>{{ $slot }}</x-type>
