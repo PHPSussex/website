ec8cfcd6127e192e5b5abcbb5b10f036ac1a3c1c
Joby Harding <code@fullstackappco.com>
2026-01-09 17:55:29 +0000

    example slide wip

---
+++ resources/views/pages/slides/example.blade.php
@@ -0,0 +1,13 @@
+<x-layout.slides title="Example Slides">
+    <x-slide>
+        <x-slide.h>Cover Slide</x-slide.h>
+        <p>TODO</p>
+    </x-slide>
+    <x-slide>
+        <x-slide.h>Slide Heading</x-slide.h>
+        <p>Some paragraph text here, what larks!</p>
+    </x-slide>
+    <x-slide>
+        <x-slide.h>Second slide</x-slide.h>
+    </x-slide>
+</x-layout.slides>
