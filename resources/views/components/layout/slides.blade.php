f7eabd319a447dd7eee89ce18998f3583c0fa9d1
Joby Harding <code@fullstackappco.com>
2026-01-10 21:54:49 +0000

    wip slide components

--- resources/views/components/layout/slides.blade.php
+++ resources/views/components/layout/slides.blade.php
@@ -1,12 +1,11 @@
 <x-layout.html {{ $attributes }}>
     <x-slot:head>
         @vite([
-            'resources/css/app.css',
             'resources/css/slides.css',
             'resources/js/slides.js',
         ])
     </x-slot:head>
-    <div class="reveal">
+    <div class="reveal transition-all">
         <div class="slides">
             {{ $slot }}
         </div>
