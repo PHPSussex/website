dbc23e0af0bbdbf8d934ff9d290ef1121776b0f2
Joby Harding <code@fullstackappco.com>
2026-01-11 10:07:52 +0000

    Remove slides

--- resources/views/pages/slides/2026-02.blade.php
+++
@@ -1,8 +0,0 @@
-<x-layout.slides title="Feb '26">
-    <x-slide>
-        <x-slide.h>First slide</x-slide.h>
-    </x-slide>
-    <x-slide>
-        <x-slide.h>Second slide</x-slide.h>
-    </x-slide>
-</x-layout.slides>
