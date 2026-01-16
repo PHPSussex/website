7117a916c6ed6d3208d8d6b493cce36a55a9e9db
Joby Harding <code@fullstackappco.com>
2026-01-16 13:14:16 +0000

    Some extra slide styles & components

---
+++ resources/views/components/icon/icon.blade.php
@@ -0,0 +1,2 @@
+@props(['w' => 640, 'h' => 640])
+<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {{ $w }} {{ $h }}" {{ $attributes->class('icon fill-current') }}>{{ $slot }}</svg>
--- resources/views/components/icon/index.blade.php
+++
@@ -1,2 +0,0 @@
-@props(['w' => 640, 'h' => 640])
-<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {{ $w }} {{ $h }}" {{ $attributes->class('fill-current') }}>{{ $slot }}</svg>
