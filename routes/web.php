f5661a975f7afd7477160632bf1c54f66ba4a346
Joby Harding <code@fullstackappco.com>
2026-01-21 18:28:30 +0000

    wip slides prefix

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
 
-$prefix = Config::get('slides.prefix', '');
-$uri = $prefix ? trim($prefix, '/').'/slides' : 'slides';
-
-Route::prefix($uri)->group(function () {
+Route::prefix('slides/'.trim(Config::get('slides.prefix', ''), '/'))->group(function () {
     Route::get('/{view}', SlidesController::class);
 });
