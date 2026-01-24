a9d7f31e0389229ff0bdebecb4a57d6edf19bb43
Joby Harding <code@fullstackappco.com>
2026-01-24 13:00:57 +0000

    Slides prefix helper

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,12 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Slides\SlidesController;
-use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
+
+use function App\slidesUrl;
 
 Route::get('/', HomeController::class)->name('home');
 
-Route::prefix('slides/'.trim(Config::get('slides.prefix', ''), '/'))->group(function () {
-    Route::get('/{view}', SlidesController::class)->where('view', '.*');
+Route::prefix(slidesUrl())->group(function () {
+    Route::get('/{view}', SlidesController::class)->where('view', '.*')->name('slides');
 });
--- tests/Feature/FunctionsTest.php
+++ tests/Feature/FunctionsTest.php
@@ -2,12 +2,14 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Support\Facades\Config;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
+use function App\slidesUrl;
 use function App\variant;
 
-class functionsTest extends TestCase
+class FunctionsTest extends TestCase
 {
     #[Test]
     public function it_resolves_single_variant(): void
@@ -72,5 +74,35 @@ class functionsTest extends TestCase
         $result = variant(' primary   bold ', $variants);
 
         $this->assertEquals(['text-blue-500', 'font-bold'], $result);
+    }
+
+    #[Test]
+    public function slides_url_returns_base_path_without_arguments(): void
+    {
+        Config::set('slides.prefix', null);
+        $this->assertEquals('slides', slidesUrl());
+
+        Config::set('slides.prefix', 'test-prefix');
+        $this->assertEquals('slides/test-prefix', slidesUrl());
+    }
+
+    #[Test]
+    public function slides_url_appends_path(): void
+    {
+        Config::set('slides.prefix', null);
+        $this->assertEquals('slides/my-presentation', slidesUrl('my-presentation'));
+
+        Config::set('slides.prefix', 'test-prefix');
+        $this->assertEquals('slides/test-prefix/my-presentation', slidesUrl('my-presentation'));
+    }
+
+    #[Test]
+    public function slides_url_trims_slashes_from_path(): void
+    {
+        Config::set('slides.prefix', null);
+        $this->assertEquals('slides/my-presentation', slidesUrl('/my-presentation/'));
+
+        Config::set('slides.prefix', 'test-prefix');
+        $this->assertEquals('slides/test-prefix/my-presentation', slidesUrl('/my-presentation/'));
     }
 }
