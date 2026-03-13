e9b1efb58fb15149036d803920001d38a186794b
Joby Harding <code@fullstackappco.com>
2026-03-13 09:02:39 +0000

    Brighten images

--- resources/views/components/home/hero.blade.php
+++ resources/views/components/home/hero.blade.php
@@ -1,5 +1,5 @@
 @use(function App\setting)
-<div {{ $attributes->class("h-[10rem] md:h-[14rem] lg:h-[18rem] xl:h-[20rem] bg-gradient-to-br from-cyan-300 to-cyan-700 overflow-hidden relative") }}>
-    {{ setting('home-hero')->img()->attributes(['class' => 'object-cover w-full min-h-full grayscale']) }}
+<div {{ $attributes->class("h-[10rem] md:h-[16rem] lg:h-[18rem] xl:h-[20rem] bg-gradient-to-br from-cyan-300 to-cyan-700 overflow-hidden relative") }}>
+    {{ setting('home-hero')->img()->attributes(['class' => 'object-cover w-full min-h-full grayscale brightness-130']) }}
     <div class="absolute inset-0 bg-gradient-to-br from-cyan-200 to-cyan-600 mix-blend-multiply"></div>
 </div>
