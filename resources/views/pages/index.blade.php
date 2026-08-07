972b14ee034be789c80b17650d6bd443c9b9f6da
Joby Harding <code@fullstackappco.com>
2026-08-07 17:19:27 +0100

    Event tweak

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -73,7 +73,7 @@
                 </x-type.para>
                 <x-type.para variant="dim">
                     PHPSussex veteran <a class="link link-focus" href="https://akrabat.com/">Rob Allen</a> on API security
-                    and PHPSuss debutant <a class="link link-focus" href="https://www.linkedin.com/in/terdelyi/">Tamas Erdelyi</a>
+                    and PeepySuss debutant <a class="link link-focus" href="https://www.linkedin.com/in/terdelyi/">Tamas Erdelyi</a>
                     on building a platform for tech communities. 🔥🔥🔥
                 </x-type.para>
                 <x-type.para variant="dim">
